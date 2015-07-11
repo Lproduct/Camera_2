ba7c257a372b8f1ff7295221f375f63aa6d00c87
Lvil <a.chambellan.11@groupe-esigelec.fr>
2015-07-11 12:56:40 +0200

    first Commit

---
+++ Camera_2.pro
@@ -0,0 +1,33 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-07-09T17:04:29
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Camera_2
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+HEADERS += \
+    Common/exampleHelper.h \
+    mvIMPACT_CPP/mvIMPACT_acquire.h \
+    mvDisplay/Include/mvIMPACT_acquire_display.h \
+    mvDeviceManager/Include/mvDeviceManager.h \
+    DriverBase/Include/mvDriverBaseEnums.h \
+    mvPropHandling/Include/mvPropHandlingDatatypes.h
+
+win32: LIBS += -L$$PWD/lib/ -lmvDeviceManager
+
+INCLUDEPATH += $$PWD/
+DEPENDPATH += $$PWD/
+
+win32:!win32-g++: PRE_TARGETDEPS += $$PWD/lib/mvDeviceManager.lib
---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <QCoreApplication>
+#include <iostream>
+#include <Common/exampleHelper.h>
+#include <mvIMPACT_CPP/mvIMPACT_acquire.h>
+#include <mvDisplay/Include/mvIMPACT_acquire_display.h>
+
+using namespace std;
+using namespace mvIMPACT::acquire;
+using namespace mvIMPACT::acquire::display;
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    DeviceManager devMgr;
+    Device* pDev = getDeviceFromUserInput( devMgr );
+    if( !pDev )
+    {
+        cout << "Unable to continue!" << endl
+             << "Press [ENTER] to end the application" << endl;
+        cin.get();
+        return 0;
+    }
+
+    try
+    {
+        pDev->open();
+    }
+    catch( const ImpactAcquireException& e )
+    {
+        // this e.g. might happen if the same device is already opened in another process...
+        cout << "An error occurred while opening the device(error code: " << e.getErrorCode() << ")." << endl
+             << "Press [ENTER] to end the application" << endl;
+        cin.get();
+        return 0;
+    }
+
+    FunctionInterface fi( pDev );
+
+    // send a request to the default request queue of the device and wait for the result.
+    fi.imageRequestSingle();
+    manuallyStartAcquisitionIfNeeded( pDev, fi );
+    // Define the Image Result Timeout (The maximum time allowed for the Application
+    // to wait for a Result). Infinity value:-1
+    const int iMaxWaitTime_ms = -1;   // USB 1.1 on an embedded system needs a large timeout for the first image.
+    // wait for results from the default capture queue.
+    int requestNr = fi.imageRequestWaitFor( iMaxWaitTime_ms );
+    manuallyStopAcquisitionIfNeeded( pDev, fi );
+    // check if the image has been captured without any problems.
+    if( !fi.isRequestNrValid( requestNr ) )
+    {
+        // If the error code is -2119(DEV_WAIT_FOR_REQUEST_FAILED), the documentation will provide
+        // additional information under TDMR_ERROR in the interface reference
+        cout << "imageRequestWaitFor failed (" << requestNr << ", " << ImpactAcquireException::getErrorCodeAsString( requestNr ) << ")"
+             << ", timeout value too small?" << endl;
+        return 0;
+    }
+
+    const Request* pRequest = fi.getRequest( requestNr );
+    if( !pRequest->isOK() )
+    {
+        cout << "Error: " << pRequest->requestResult.readS() << endl;
+        // if the application wouldn't terminate at this point this buffer HAS TO be unlocked before
+        // it can be used again as currently it is under control of the user. However terminating the application
+        // will free the resources anyway thus the call
+        // fi.imageRequestUnlock( requestNr );
+        // can be omitted here.
+        return 0;
+    }
+
+    cout << "Image captured( " << pRequest->imagePixelFormat.readS() << " " << pRequest->imageWidth.read() << "x" << pRequest->imageHeight.read() << " )" << endl;
+    // unlock the buffer to let the driver know that you no longer need this buffer.
+    fi.imageRequestUnlock( requestNr );
+
+    cout << "Press [ENTER] to end the application" << endl;
+    cin.get();
+
+    return a.exec();
+}
